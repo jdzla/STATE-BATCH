cafa5e5a15a2ef6a017d7c9dd8590afc22620a05
Ryo Ueda <u483198f@gmail.com>
2022-10-31 20:14:10 +0900

    set debug mode of example to True

--- examples/single_atom/run.py
+++ examples/single_atom/run.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('../../statebatch/')
 from statebatch.STATEbatch import Batch
 
-Batch_obj = Batch('input.yaml')
+Batch_obj = Batch('input.yaml', debug=True)
 
 Batch_obj.prerun(make_jobscript='serial')
 # serial
